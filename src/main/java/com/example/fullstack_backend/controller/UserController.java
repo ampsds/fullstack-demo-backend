314a7d8584fbfe56f1bd3bf5ee28addd19e0645f
anyou4432 <kyoandya@gmail.com>
2024-06-27 01:33:07 +0900

    add getmapping users

--- src/main/java/com/example/fullstack_backend/controller/UserController.java
+++ src/main/java/com/example/fullstack_backend/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.example.fullstack_backend.controller;
 
-import com.example.fullstack_backend.model.User;
-import com.example.fullstack_backend.repository.UserRepository;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.fullstack_backend.model.User;
+import com.example.fullstack_backend.repository.UserRepository;
 
 @RestController
 public class UserController {
@@ -18,6 +21,12 @@ public class UserController {
   User newUser(@RequestBody User newUser) {
     return userRepository.save(newUser);
   }
+
+  @GetMapping("/users")
+  List<User> getAllUsers() {
+    return userRepository.findAll();
+  }
+
 
   
 }
